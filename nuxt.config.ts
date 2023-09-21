facf58ac7a418158506bf9feb08911db695cfc3d
Anastasia Epova <1epova.aa@gmail.com>
2023-09-21 14:52:18 +0300

    [~] preparation for deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,9 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/assets/styles/index.scss"],
+  ssr: false,
+  target: "static",
+  router: {
+    base: "/modalComponent/"
+  }
 })
